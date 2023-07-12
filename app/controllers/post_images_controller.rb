06ca4a81252e837e8899c4b2f41b4532647f9647
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-07-12 12:24:13 +0000

    [add] 4-11 [index/画像エラー解消]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,27 +1,28 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_new_image = PostImage.new
   end
-  
+
   def create
     @post_new_image = PostImage.new(post_image_params)
     @post_new_image.user_id = current_user.id
     @post_new_image.save
     redirect_to post_images_path
-    
+
   end
 
   def index
+    @post_index_images = PostImage.all
   end
 
   def show
   end
-  
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
