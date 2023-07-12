bda3cd2f41b3a952c1e697bacc415e8e71851d3f
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-07-12 18:28:23 +0000

    [add] 4-16 [updateとeditビューのリンク完成]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
   def edit
     @edit_user = User.find(params[:id])
   end
+  
+  def update
+    @edit_user = User.find(params[:id])
+    @edit_user.update (user_params)
+    redirect_to user_path(@edit_user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
 end
